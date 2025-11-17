427e692bee0d0380634f55b02a6efedbf8c3bc51
ammar11 <110504408+ammarsaleh111@users.noreply.github.com>
2025-11-17 18:57:38 +0200

    chore: Prepare Home component for future enhancements and optimizations

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -360,12 +360,9 @@ const Home = () => {
       >
         <div className="container">
           <div className="section-header">
-            <h2 id="top-selling-title" className="section-title">
+            <h2 id="top-selling-title " className="section-title">
               Our Top Selling Products
             </h2>
-            <Link className="link-all" to="/shop">
-              View All
-            </Link>
           </div>
           <div className="top-selling-grid">
             {topSellingProducts.map((product, index) => {
